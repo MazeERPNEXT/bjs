c15f21f8122b34f4290d9887b4a8cb14db6d75b0
jeyanth <jeyanthkumar@mazeworkssolutions.com>
2024-04-23 18:02:03 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## BJs Raw Pet Food
+
+Frappe scripts for BJs Raw Pet Food
+
+#### License
+
+mit
---
+++ bjs/hooks.py
@@ -0,0 +1,10 @@
+app_name = "bjs"
+app_title = "BJs Raw Pet Food"
+app_publisher = "Avunu LLC"
+app_description = "Frappe scripts for BJs Raw Pet Food"
+app_email = "mail@avu.nu"
+app_license = "mit"
+required_apps = [
+    "erpnext",
+    "shipstation_integration"
+]
